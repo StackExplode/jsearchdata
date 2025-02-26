94731688425874dbbd1318463f1f96789fe0dd78
寧々 <nene@jloli.cc>
2025-02-26 14:21:58 +0800

    Update test.lua

--- Action/test.lua
+++ Action/test.lua
@@ -1,12 +1,58 @@
 local json = require("JSON")
 
--- Lua 脚本示例：输出调试信息到 GitHub Actions 日志
-os.execute("echo '::notice::Starting the Lua script execution.'")  -- 调试信息
-print("inner print")
-local file = io.open("carddata.json", "r")
-local content = file:read("*all")
-local data_json = json:decode(content)
-print(data_json[1]["name"])
+local fname = "d_scheduel.json"
 
-os.execute("echo '::notice::Bye!'")  -- 调试信息
+os.execute("echo '::notice::Script start!'")
+
+function GetTimeStampFromStr(date_str)
+
+-- 提取年、月、日
+local year, month, day = date_str:match("^(%d+)-(%d+)-(%d+)$")
+
+-- 将它们转换为数字
+year = tonumber(year)
+month = tonumber(month)
+day = tonumber(day)
+
+-- 创建一个日期表
+local date_table = {
+    year = year,
+    month = month,
+    day = day,
+    hour = 0,   -- 设置为00:00:00
+    min = 0,
+    sec = 0,
+    isdst = false  -- 不考虑夏令时
+}
+
+-- 将日期表转化为时间戳
+ return os.time(date_table)
+end
+
+
+
+local file = io.open(fname, "r")
+if not file then 
+	os.execute("echo '::error::File not found!'")
+	return
+end
+local content = file:read("*all")
+file:close()
+local jjj = json:decode(content)
+local now = os.time()
+
+for k,v in pairs(jjj) do
+	local t = GetTimeStampFromStr(v["date"])
+	if now - t > 24*3600 then
+		jjj[k] = nil
+	end
+end
+
+local rst = json:encode_pretty(jjj)
+
+file = io.open(fname, "w")
+file:write(rst)
+file:close()
+
+os.execute("echo '::notice::Script finished!'")
 
