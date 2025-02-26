886fbf9248b7bb01c28587c086205258d5603bc5
寧々 <nene@jloli.cc>
2025-02-26 11:56:10 +0800

    Update test.lua

--- Action/test.lua
+++ Action/test.lua
@@ -1,5 +1,12 @@
+local json = require "json"
+
 -- Lua 脚本示例：输出调试信息到 GitHub Actions 日志
-os.execute("echo '::debug::Starting the Lua script execution.'")  -- 调试信息
+os.execute("echo '::notice::Starting the Lua script execution.'")  -- 调试信息
 print("inner print")
-os.execute("echo '::error::Test Error.'") 
-os.execute("echo '::warning::Bye!'") 
+local file = io.open("carddata.json", "r")
+local content = file:read("*all")
+local data_json = json:decode(content)
+print(data_json[1]["name"])
+
+os.execute("echo '::notice::Bye!'")  -- 调试信息
+
